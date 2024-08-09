2cfa8999069d20f5d416c32fad0acbbf3c2ea4ae
stopstone <wltjr6432@naver.com>
2024-08-09 16:35:21 +0900

    feat: HomeActivity Navigation 추가

---
+++ app/src/main/java/com/stopstone/githubsearchpractice/view/search/SearchFragment.kt
@@ -0,0 +1,33 @@
+package com.stopstone.githubsearchpractice.view.search
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.stopstone.githubsearchpractice.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("SearchFragment", "onViewCreated")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
